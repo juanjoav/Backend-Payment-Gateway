8f3fa01146365e2e3697abe9389ac02d29694b6d
JuanJ <juanjoav010@gmail.com>
2025-05-17 18:50:06 -0500

    modulos necesarios y division

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
