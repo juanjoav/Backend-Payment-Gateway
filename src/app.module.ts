1aafaaa5614fe67ed10dfb0f5584ad725710b96f
JuanJ <juanjoav010@gmail.com>
2025-05-17 19:05:33 -0500

    conexion inicial base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule, TransactionsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_DATABASE'),
+      autoLoadEntities: true, 
+      // synchronize: process.env.NODE_ENV !== 'production', 
+    }),
+    inject: [ConfigService],
+    }),
+    ProductsModule, 
+    TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
